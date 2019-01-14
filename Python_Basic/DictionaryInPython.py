e649ddca98bfdf4540ec5fe9a205703c506e429b
Prodip Sarker <prodip_sarker@diu.edu.bd>
2019-01-14 23:59:00 +0600

    Add files via upload

---
+++ Python_Basic/DictionaryInPython.py
@@ -0,0 +1,14 @@
+#
+# marks= {"Bangla": 68,"English":78,"Math":98}
+# print(marks["Bangla"])
+#
+# for i in marks:
+#     print(i)
+
+# my_marks = {"B":[45,56,78],"E":[45,65,78],"M":[78,98,99]}
+#
+# print(my_marks["M"])
+
+my_nums = {1 : 1, 2 : 4, 3 : 9, 4 : 16}
+print(my_nums.get(5, "5 not in my numbers!"))
+
---
+++ Python_Basic/NOne.py
@@ -0,0 +1,41 @@
+# print(None ==0)
+# print(None == False)
+# None==False
+# None == True
+
+# None == TRue
+# None == True
+# None==0
+# None ==[]
+# a=None
+# a==None
+#
+# print(a)
+#
+# def myFunc(x):
+#     if x:
+#         return x *x
+#     else:
+#         return 0
+#
+#
+# print(myFunc())
+
+
+# def myFunc(x=None):
+#     if x:
+#         return x *x
+#     else:
+#         return 0
+#
+# print(myFunc())
+# print(myFunc(4))
+
+def fnc():
+    print('Printing Hello')
+
+
+what_i_get = fnc()
+
+print(fnc())
+print(fnc())
