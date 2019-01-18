8ed1e86c3ba0dc70163f0b0af4fc307f6bf9bf9f
Prodip Sarker <prodip_sarker@diu.edu.bd>
2019-01-18 12:54:04 +0600

    Python

---
+++ Python_Basic/FunctionInDeeply.py
@@ -0,0 +1,23 @@
+# def Get_Square_Root(x):
+#     """
+#     :param x: x must be a number
+#     :return: the square root of x.
+#     """
+#     return x * x
+#     pass
+#
+#     print(Get_Square_Root(5))
+# print(Get_Square_Root(8))
+#
+
+# import random
+# value = random.randint(1, 100)
+# print(value)
+#
+# from math import pi,sqrt
+# print(pi)
+# print(sqrt(6))
+
+from math import sqrt as Square
+
+print(Square(9))
